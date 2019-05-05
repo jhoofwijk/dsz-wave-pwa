129450620e9d1426e176d68efdb5581f27621ebd
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2019-05-05 20:10:08 +0200

    display practice properties

--- src/components/Training.jsx
+++ src/components/Training.jsx
@@ -4,10 +4,25 @@ import { Card, CardContent } from '@material-ui/core';
 
 
 export default function Training(props) {
+    console.log(props.practice);
+    const { practice } = props;
     return (
         <Card style={{margin: "10px 0"}}>
             <CardContent>
-                Hello world
+                <div style={{display: 'flex'}}>
+                    <div style={{flexGrow: 1}}>
+                            {practice.start} - {practice.end}
+                    </div>
+                    <div>
+                        <strong>
+                            {practice.enrolled} / {practice.allowed}
+                        </strong>
+                    </div>
+                </div>
+                <div style={{display: 'flex'}}>
+                    <div style={{flexGrow: 1}}>{practice.date}</div>
+                    <div>{practice.location}</div>
+                </div>
             </CardContent>
         </Card>
     )
