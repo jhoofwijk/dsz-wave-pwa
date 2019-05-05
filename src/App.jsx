8eb28939121c7f10435fad0d2cdf0ba89a445bdf
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2019-05-05 19:04:19 +0200

    add theme and remove non-functional buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React, { useEffect } from 'react';
 import Home from './pages/Home';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main: '#001aff',
+        // contrastText: 'yellow',
+    },
+    secondary: {
+        main: '#FFFF00',
+    },
+  },
+});
+
 export default function App(props) {
     useEffect(() => {
         document.getElementById('shell').style.opacity = 0;
     }, []);
 
-    return <Home />;
+    return (
+        <MuiThemeProvider theme={theme}>
+            <Home />
+        </MuiThemeProvider>
+    );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 export default function Home(props) {
     return (
         <>
-            <Header />
+            <Header title="Trainingen" />
             Hello world
         </>
     )
