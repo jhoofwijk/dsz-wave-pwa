1ddbf33c0ebfbdad5741fa104735664e5d97cdd7
Jorn Hoofwijk <jornhoofwijk@gmail.com>
2019-10-22 15:41:24 +0200

    remove unused imports

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,13 +1,6 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React from "react";
 
 import Edit from "@material-ui/icons/Edit";
-import withStyles from "@material-ui/core/styles/withStyles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import Home from "@material-ui/icons/Home";
 
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
